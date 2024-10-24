626dc7dc6f2983a90480114a2ec8f1fb3e2a6bc4
santi <santi2811b@gmail.com>
2024-10-24 17:57:51 -0600

    first leetcode

---
+++ reverse_string.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        i = 0
+        j = len(s)-1
+        while(i < j):
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+            i+=1
+            j-=1;
+        """
+        Do not return anything, modify s in-place instead.
+        """
+solution = Solution()
+
+# Prepare the test case
+test_case = ['h', 'e', 'l', 'l', 'o']
+
+# Call the reverseString method
+solution.reverseString(test_case)
+
+# Print the modified list
+print(test_case)  # Output should be ['o', 'l', 'l', 'e', 'h']
